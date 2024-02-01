db44d80c9da85d1e66fff24c28ba5ce64bea8b88
Yaroslav Mykhalchuk <ipzm222_myaa@student.ztu.edu.ua>
2024-02-01 23:47:13 +0200

    add seeders

--- database/seeders/TypeRepairSeeder.php
+++ database/seeders/TypeRepairSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\TypeRepair;
 
 class TypeRepairSeeder extends Seeder
 {
@@ -15,38 +16,31 @@ class TypeRepairSeeder extends Seeder
         $typeRepairs = [
             [
                 'name' => 'Комплексна діагностика',
-                'type_service_id' => 1,
-                'price_id' => 1
+                'type_service_id' => 1
             ],
             [
                 'name' => 'Ремонт або заміна клавіатури',
-                'type_service_id' => 1,
-                'price_id' => 2
+                'type_service_id' => 1
             ],
             [
                 'name' => 'Заміна роз’ємів(USB, живлення та ін.)',
-                'type_service_id' => 1,
-                'price_id' => 3
+                'type_service_id' => 1
             ],
             [
                 'name' => 'Заміна корпусу та його частин',
-                'type_service_id' => 1,
-                'price_id' => 4
+                'type_service_id' => 1
             ],
             [
                 'name' => 'Ремонт материнських плат, заміна мостів та інших мікросхем',
-                'type_service_id' => 1,
-                'price_id' => 5
+                'type_service_id' => 1
             ],
             [
                 'name' => 'Заміна комплектуючих (процесорів, HDD, оптичних приводів, пам’яті та ін.)',
-                'type_service_id' => 1,
-                'price_id' => 6
+                'type_service_id' => 1
             ],
             [
                 'name' => 'Заміна акумуляторів та блоків живлення',
-                'type_service_id' => 1,
-                'price_id' => 7
+                'type_service_id' => 1
             ]        
         ];
 
